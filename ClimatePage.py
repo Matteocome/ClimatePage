5d501b41c6bb26c41b3530c829f9cde2739a2d21
Matteocome <34767200+Matteocome@users.noreply.github.com>
2023-08-12 13:08:04 +0100

    Update ClimatePage.py

--- ClimatePage.py
+++ ClimatePage.py
@@ -1,7 +1,6 @@
 # cliamte page mind-map
 
 import streamlit as st
-import seaborn as sns
 import networkx as nx
 import matplotlib.pyplot as plt
 from streamlit import GraphvizChart
@@ -13,33 +12,58 @@ import altair as alt
 st.write("Hello")
 
 ## Testing code:
-def create_mind_map():
-    G = nx.DiGraph()
+import streamlit as st
+import graphviz
 
-    st.sidebar.header("Add Nodes")
-
-    with st.sidebar.form("add_node_form"):
-        new_node = st.text_input("Enter Node Name:")
-        parent_node = st.selectbox("Select Parent Node:", ["None"] + list(G.nodes))
-
-        submitted = st.form_submit_button("Add Node")
-
-        if submitted and new_node:
-            G.add_node(new_node)
-            if parent_node != "None":
-                G.add_edge(parent_node, new_node)
-
-    pos = nx.spring_layout(G)  # Positioning of nodes
-
-    plt.figure(figsize=(8, 6))
-    nx.draw(G, pos, with_labels=True, font_weight='bold', node_size=2000)
-    st.pyplot(plt)
+def generate_mind_map(node, parent=None):
+    dot = graphviz.Digraph(format="png")
+    
+    if parent:
+        dot.node(parent)
+        dot.node(node)
+        dot.edge(parent, node)
+    else:
+        dot.node(node)
+    
+    return dot
 
 def main():
     st.title("Mind Map Generator")
     st.write("Create a simple mind map by adding nodes.")
 
-    create_mind_map()
+    st.sidebar.header("Add Nodes")
+
+    with st.sidebar.form("add_node_form"):
+        new_node = st.text_input("Enter Node Name:")
+        parent_node = st.selectbox("Select Parent Node:", ["None"] + [node for node, _ in nodes])
+
+        submitted = st.form_submit_button("Add Node")
+
+        if submitted and new_node:
+            nodes.append((new_node, parent_node))
+    
+    st.sidebar.subheader("Generated Mind Map")
+
+    for node, parent_node in nodes:
+        st.sidebar.write(f"Node: {node}, Parent: {parent_node}")
+    
+    st.sidebar.write("Click the button below to generate the mind map:")
+
+    generate_map = st.button("Generate Mind Map")
+
+    if generate_map:
+        dot = graphviz.Digraph(format="png")
+        for node, parent_node in nodes:
+            if parent_node != "None":
+                dot.node(parent_node)
+                dot.node(node)
+                dot.edge(parent_node, node)
+            else:
+                dot.node(node)
+        
+        st.graphviz_chart(dot)
 
 if __name__ == "__main__":
+    nodes = []  # List to store node information (node, parent)
     main()
+
