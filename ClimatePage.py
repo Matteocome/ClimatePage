9916795e87eb7bdb57522263d1bc69d5a829c1e0
Matteocome <34767200+Matteocome@users.noreply.github.com>
2023-08-12 12:57:38 +0100

    Create ClimatePage.py

---
+++ ClimatePage.py
@@ -0,0 +1,13 @@
+# cliamte page mind-map
+
+import streamlit as st
+import seaborn as sns
+import networkx as nx
+import matplotlib.pyplot as plt
+from streamlit import GraphvizChart
+from PIL import Image
+import pandas as pd 
+import numpy as np
+import altair as alt
+
+st.write("Hello")
